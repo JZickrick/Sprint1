771a7027c8b0d1febaa1b9d763bc0d0dfdf40a16
levertyk <levertyk4517@my.uwstout.edu>
2022-04-28 11:05:27 -0500

    Updated PHP

--- AccountWebpage.php
+++ AccountWebpage.php
@@ -1,9 +1,25 @@
-<html lang="en">
-	<body>
+$servername = "localhost";
+$username = "user1";
+$password = "pwd16";
+$dbname = "AgileExpG1";
 
-		<?php
-		echo $_POST["newEmail"];
-		?>
-	</body>
-</html>
-	 
+try {
+
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $stmt = $conn->prepare("SELECT Email FROM Owner WHERE OwnerID = 1");
+
+    $stmt->execute();
+
+	$result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+	echo $stmt->fetchAll();
+#  }
+}
+catch(PDOException $e) {
+    echo "Error: " . $e->getMessage();
+}
+$conn = null;
+header('Location: diverAdd.html');
+
+
+?>
