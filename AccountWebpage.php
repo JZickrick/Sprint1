c6b7c91396b321db51d46b8f6c57019a3e2b7b8e
Keenan Leverty <levertyk4517@my.uwstout.edu>
2022-04-30 19:27:02 -0500

    Updates

--- AccountWebpage.php
+++ AccountWebpage.php
@@ -1,33 +1,6 @@
 
 <?php
 
-// $servername = "localhost";
-// $username = "user1";
-// $password = "pwd16";
-// $dbname = "AgileExpG1";
-
-// echo "ahhhhh";
-
-// try {
-
-//     $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-//     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-//     $stmt = $conn->prepare("SELECT Email FROM Owner WHERE OwnerID = 1");
-
-//     $stmt->execute();
-
-// 	$result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
-// 	echo $result->fetchAll();
-// 	echo "<it got to here ig";
-// }
-// catch(PDOException $e) {
-//     echo "Error: " . $e->getMessage();
-// }
-
-// echo "that all didn't work eh";
-// $conn = null;
-// header('Location: AccountWebpage.html');
-
 $servername = "localhost";
 $username = "user1";
 $password = "pwd16";
