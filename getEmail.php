df34281fc2fd0b3e1989ef989e23bd0b8cec807e
levertyk <levertyk4517@my.uwstout.edu>
2022-04-30 19:40:46 -0500

    Added more php integration the html

--- getEmail.php
+++ getEmail.php
@@ -7,7 +7,6 @@ $password = "pwd16";
 $dbname = "AgileExpG1";
 
 try {
-	echo "doing stuff yo\n";
 
 	$conn = new PDO("mysql:host=$servername;dbname=$dbnam", $username, $password);
 	$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
@@ -15,7 +14,6 @@ try {
 	$stmt->execute();
 	
 	echo $stmt->fetch()[0];
-	echo "\nSuccess";
 } catch (PDOException $e) {
 	echo "Error: " . $e->getMessage();
 }
--- getName.php
+++ getName.php
@@ -7,15 +7,13 @@ $password = "pwd16";
 $dbname = "AgileExpG1";
 
 try {
-	echo "doing stuff yo\n";
 
 	$conn = new PDO("mysql:host=$servername;dbname=$dbnam", $username, $password);
 	$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-	$stmt = $conn->prepare("SELECT Email FROM AgileExpG1.Owner WHERE OwnerID = 1");
+	$stmt = $conn->prepare("SELECT Name FROM AgileExpG1.Owner WHERE OwnerID = 1");
 	$stmt->execute();
 	
 	echo $stmt->fetch()[0];
-	echo "\nSuccess";
 } catch (PDOException $e) {
 	echo "Error: " . $e->getMessage();
 }
