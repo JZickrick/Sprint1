a583d37ec7903fa28daf59b5761119ef35e9ff82
coez8824 <coez8824@my.uwstout.edu>
2022-05-02 18:12:20 -0500

    Made everything look fancy schmancy (aka "modern")
    Hopefully nothing breaks

---
+++ getName.php
@@ -0,0 +1,21 @@
+
+<?php
+
+$servername = "localhost";
+$username = "user1";
+$password = "pwd16";
+$dbname = "AgileExpG1";
+
+try {
+
+	$conn = new PDO("mysql:host=$servername;dbname=$dbnam", $username, $password);
+	$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	$stmt = $conn->prepare("SELECT Name FROM AgileExpG1.Owner WHERE OwnerID = 1");
+	$stmt->execute();
+	
+	echo $stmt->fetch()[0];
+} catch (PDOException $e) {
+	echo "Error: " . $e->getMessage();
+}
+
+?>
